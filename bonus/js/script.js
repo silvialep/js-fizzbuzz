bb8614c9c892e618e5a61347cc12a785b4f2b1fb
silvialep <silvia.lepore_sl@libero.it>
2023-02-24 15:37:55 +0100

    feat: created divs for each instance using js + style with css

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -11,24 +11,46 @@ let fizz = 'Fizz';
 let buzz = 'Buzz';
 let fizzBuzz = 'FizzBuzz';
 
+/* assegnazione dell'id container dall'html */
+let containerEl = document.getElementById('container');
+
+
 /* programma che stampa in console i numeri da 1 a 100 */
 for (let i = 1; i <= 100; i++) {
-    
+
+    /* creazione di nuovo elemento in html */
+    let newDiv = document.createElement('div');
+    containerEl.append(newDiv);
+
     /* per i multipli sia di 3 che di 5 stampa "FizzBuzz" al posto del numero */
     if (i % 3 == 0 && i % 5 == 0) {
         console.log(fizzBuzz);
+        containerEl.append(newDiv);
+        newDiv.innerHTML= fizzBuzz;
     } else
+
     /* per i multipli solo di 3 stampa "Fizz" al posto del numero */
     if (i % 3 == 0) {
         console.log(fizz);
+        containerEl.append(newDiv);
+        newDiv.innerHTML = fizz;
     } else
+
     /* per i multipli solo di 5 stampa "Buzz" al posto del numero */
     if (i % 5 == 0) {
         console.log(buzz);
+        containerEl.append(newDiv);
+        newDiv.innerHTML = buzz;
     }
-
+    
     /* per tutti gli altri numeri stampa solo il numero */
     else {
-        console.log(contatore = i);
+        console.log(i);
+        containerEl.append(newDiv);
+        newDiv.innerHTML = i;
     }
+
+    newDiv.style.border = '1px solid black';
+    newDiv.style.width = 'calc(100% / 7 - (20px / 6))';
 }
+
